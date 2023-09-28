5529d2e7bb8b57f06067cbe3488d487cefbce7db
ayanonline <ayanghosh1389@gmail.com>
2023-09-28 14:49:51 +0530

    jwt option changed

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,6 +10,7 @@ const sendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
     secure: true,
+    sameSite: "None",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
